9f0e8c0e54218bb1ac39875d79da7db5f59e8783
ReadyStack <jmshinhwa@naver.com>
2026-09-25 09:17:19 +0000

    jetbrains source actions-deprecation-lint-2026

---
+++ actions-deprecation-lint-2026/jetbrains/src/main/java/com/getreadystack/p_actions_deprecation_lint_2026/RsCheckAction.java
@@ -0,0 +1,33 @@
+package com.getreadystack.p_actions_deprecation_lint_2026;
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import org.jetbrains.annotations.NotNull;
+
+public class RsCheckAction extends AnAction {
+  @Override public @NotNull ActionUpdateThread getActionUpdateThread() { return ActionUpdateThread.BGT; }
+
+  @Override public void update(@NotNull AnActionEvent e) {
+    VirtualFile vf = e.getData(CommonDataKeys.VIRTUAL_FILE);
+    e.getPresentation().setEnabledAndVisible(e.getProject() != null && vf != null && !vf.isDirectory());
+  }
+
+  @Override public void actionPerformed(@NotNull AnActionEvent e) {
+    Project p = e.getProject();
+    VirtualFile vf = e.getData(CommonDataKeys.VIRTUAL_FILE);
+    if (p == null || vf == null || vf.isDirectory()) return;
+    RsAnnotator.ON.add(vf.getPath());
+    RsAnnotator.TELL.add(vf.getPath());
+    FileEditorManager.getInstance(p).openFile(vf, true);
+    PsiFile pf = PsiManager.getInstance(p).findFile(vf);
+    if (pf != null) DaemonCodeAnalyzer.getInstance(p).restart(pf);
+  }
+}
